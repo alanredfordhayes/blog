07b021232e54ab2e0088a3d89f356209789ec26c
Alan Hayes <ahayes@dotdash.com>
2022-12-03 12:01:44 -0500

    amazong cognito configuration

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -7,9 +7,17 @@ resource "aws_cognito_user_pool" "pool_1" {
       priority = 1
     }
 
-    recovery_mechanism {
+  recovery_mechanism {
       name     = "verified_phone_number"
       priority = 2
     }
   }
+
+  password_policy {
+    minimum_length = 8
+    require_lowercase = true
+    require_uppercase = true
+    require_symbols = true 
+  }
+
 }
