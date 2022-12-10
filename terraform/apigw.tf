e766f5d7739c89b98a7f720834a7b77efd72a081
Alan Hayes <ahayes@dotdash.com>
2022-12-10 13:27:43 -0500

    iterate post method

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -1,18 +1,19 @@
 resource "aws_api_gateway_rest_api" "api_1" {
-  name = local.aws_api_gateway_rest_api__api_1__name
-  endpoint_configuration { types = local.aws_api_gateway_rest_api__api_1__endpoint_configuration }
+    name = local.aws_api_gateway_rest_api__api_1__name
+    endpoint_configuration { types = local.aws_api_gateway_rest_api__api_1__endpoint_configuration }
 }
 
 resource "aws_api_gateway_resource" "resources" {
-  for_each    = toset(local.site_pages)
-  parent_id   = aws_api_gateway_rest_api.api_1.root_resource_id
-  path_part   = each.value
-  rest_api_id = aws_api_gateway_rest_api.api_1.id
+    for_each    = toset(local.site_pages)
+    parent_id   = aws_api_gateway_rest_api.api_1.root_resource_id
+    path_part   = each.value
+    rest_api_id = aws_api_gateway_rest_api.api_1.id
 }
 
 resource "aws_api_gateway_method" "post" {
-  authorization = local.aws_api_gateway_method__post__authorization
-  http_method   = local.aws_api_gateway_method__post__http_method
-  resource_id   = aws_api_gateway_resource.resources[each.key]
-  rest_api_id   = aws_api_gateway_rest_api.api_1.id
+    for_each = toset([for resource in aws_api_gateway_resource: resource.path_part])
+    authorization = local.aws_api_gateway_method__post__authorization
+    http_method   = local.aws_api_gateway_method__post__http_method
+    resource_id   = aws_api_gateway_resource.resources[each.key].id
+    rest_api_id   = aws_api_gateway_rest_api.api_1.id
 }
