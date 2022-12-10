bf4bb8683d22414e3d8a3b850a88514277b34498
Alan Hayes <ahayes@dotdash.com>
2022-12-10 13:20:10 -0500

    api

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -11,8 +11,8 @@ resource "aws_api_gateway_resource" "resources" {
 }
 
 resource "aws_api_gateway_method" "post" {
-  authorization = local.aws_api_gateway_method_authorization
-  http_method   = local.aws_api_gateway_method_http_method
-  resource_id   = aws_api_gateway_resource.resources.id
+  authorization = local.aws_api_gateway_method__post__authorization
+  http_method   = local.aws_api_gateway_method__post__http_method
+  resource_id   = aws_api_gateway_resource.resources[each.key]
   rest_api_id   = aws_api_gateway_rest_api.api_1.id
 }
