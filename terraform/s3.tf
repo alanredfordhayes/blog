47404f06b3699ba1d4d483fc7b67f8681eaaa0d2
Alan Hayes <ahayes@dotdash.com>
2022-12-11 17:57:43 -0500

    archive and bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,13 +1,13 @@
 data "archive_file" "files" {
     for_each = toset(local.site_pages)
     type = local.archive_file__files__type
-    source_dir  = format("$s/%s", path.module, [each.key])
-    output_path = format("$s/%s.zip", path.module, [each.key])
+    source_dir  = join("${path.module}/", [each.key] )
+    output_path  = join("${path.module}/", [each.key], ".zip")
 
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = local.project_name
-  force_destroy = true
+    bucket_prefix = local.project_name
+    force_destroy = true
 }
 
