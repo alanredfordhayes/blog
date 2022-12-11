0db799c5752252cd2d7dd19f000bc0a98db83c70
Alan Hayes <ahayes@dotdash.com>
2022-12-11 17:41:52 -0500

    archive and bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,8 @@
 data "archive_file" "files" {
     for_each = toset(local.site_pages)
     type = local.archive_file__files__type
-    source_dir  = format("${path.module}/%s", settings.value)
-    output_path = format("${path.module}/%s.zip", settings.value)
+    source_dir  = format("${path.module}/%s", [each.value])
+    output_path = format("${path.module}/%s.zip", [each.value])
 
 }
 
