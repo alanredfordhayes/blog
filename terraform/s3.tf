b4e7cd4a809ba049dd4cbd822439d4867e512118
Alan Hayes <ahayes@dotdash.com>
2022-12-11 20:19:07 -0500

    archive and bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -20,6 +20,6 @@ resource "aws_s3_object" "objects" {
     tags = {
         source_dir = "${each.value.source_dir}",
         output_path = "${each.value.output_path}",
-        site = trimprefix("${each.value.source_dir}", "./"
+        site = trimprefix("${each.value.source_dir}", "./")
     }
 }
