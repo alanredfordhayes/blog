5b76a4ccc5272bd168a1751cfa65fa74758eb300
Alan Hayes <ahayes@dotdash.com>
2022-12-12 15:06:25 -0500

    archive and bucket

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,21 +1,31 @@
 resource "aws_iam_role" "roles" {
-    name = "${local.project_name}_Role"
+    for_each = aws_s3_object.objects
+    name = "${local.project_name}__Role__${each.value.tags.key}"
     assume_role_policy = local.aws_iam_role__roles__asume_role_policy
+    tags = {
+        site = each.value.tags.key
+    }
 }
 
 resource "aws_iam_role_policy_attachment" "role_attachments" {
-    role       = aws_iam_role.roles.name
+    for_each   = aws_iam_role.roles
+    role       = each.value.name
     policy_arn = local.aws_iam_role_policy_attachment__policy_attachments__policy_arn
 }
 
 resource "aws_iam_policy" "policy" {
-    name = "${local.project_name}_Policy"
+    for_each   = aws_iam_role.roles
+    name = "${local.project_name}_Policy_${each.value.tags.site}"
     path = local.aws_iam_policy_path
     policy = local.aws_iam_policy__policies__policy
+    tags = {
+        site = each.value.tags.site
+    }
 }
 
 resource "aws_iam_policy_attachment" "policy_attachments" {
-    name = "${local.project_name}_Policy"
+    for_each = aws_iam_policy.policy
+    name = "${local.project_name}_Policy_${each.value.tags.site}"
     roles = [aws_iam_role.roles.name]
-    policy_arn = aws_iam_policy.policy.arn
+    policy_arn = aws_iam_policy.policy[each.key].arn
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,16 +1,16 @@
-resource "aws_lambda_function" "lambdas" {
-    for_each = aws_s3_object.objects
+# resource "aws_lambda_function" "lambdas" {
+#     for_each = aws_s3_object.objects
 
-    function_name = "${local.project_name}/${each.value.key}"
-    description = "${local.project_name}/${each.value.key}"
-    publish = true
+#     function_name = "${local.project_name}/${each.value.key}"
+#     description = "${local.project_name}/${each.value.key}"
+#     publish = true
 
-    s3_bucket = aws_s3_bucket.bucket.id
-    s3_key    = each.value.key
-    source_code_hash = data.archive_file.files[each.value.tags.key].output_base64sha256
+#     s3_bucket = aws_s3_bucket.bucket.id
+#     s3_key    = each.value.key
+#     source_code_hash = data.archive_file.files[each.value.tags.key].output_base64sha256
     
-    runtime = local.aws_lambda_function__lambdas__runtime
-    handler = local.aws_lambda_function__lambdas__handler
+#     runtime = local.aws_lambda_function__lambdas__runtime
+#     handler = local.aws_lambda_function__lambdas__handler
 
-    role = aws_iam_role.roles.arn
-}
+#     role = aws_iam_role.roles.arn
+# }
