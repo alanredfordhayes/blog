a5376e1c0f03c9cc52a607700b2ea8693c25bece
Alan Hayes <ahayes@dotdash.com>
2022-12-11 20:24:41 -0500

    archive and bucket

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -44,7 +44,7 @@ locals {
     )
 
     aws_iam_role_policy_attachment__policy_attachments__policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-    aws_iam_policy__polices__policy = jsonencode(
+    aws_iam_policy__policies__policy = jsonencode(
         {
           Version = "2012-10-17"
           Statement = [
