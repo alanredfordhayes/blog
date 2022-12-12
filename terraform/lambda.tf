9b087269f7fbeab16fae3d2ef32ad81bac1927a1
Alan Hayes <ahayes@dotdash.com>
2022-12-12 15:12:58 -0500

    archive and bucket

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,13 @@
 resource "aws_lambda_function" "lambdas" {
     for_each = aws_s3_object.objects
 
-    function_name = "${local.project_name}/${each.value.key}"
-    description = "${local.project_name}/${each.value.key}"
+    function_name = "${local.project_name}/${each.value.tags.site}"
+    description = "${local.project_name}/${each.value.tags.site}"
     publish = true
 
     s3_bucket = aws_s3_bucket.bucket.id
     s3_key    = each.value.key
-    source_code_hash = data.archive_file.files[each.value.tags.key].output_base64sha256
+    source_code_hash = data.archive_file.files[each.value.tags.site].output_base64sha256
     
     runtime = local.aws_lambda_function__lambdas__runtime
     handler = local.aws_lambda_function__lambdas__handler
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -20,6 +20,7 @@ resource "aws_s3_object" "objects" {
     tags = {
         source_dir = "${each.value.source_dir}",
         output_path = "${each.value.output_path}",
+        site = trimprefix("${each.value.source_dir}", "./")
         key = "${each.key}"
     }
 }
